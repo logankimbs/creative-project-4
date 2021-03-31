9f8e954621e94840252d7549c70d055dd261b099
Logan Kimball <logankimbs@gmail.com>
2021-03-31 10:20:05 -0600

    Connected to database

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -8,8 +9,15 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+mongoose.connect('mongodb://localhost:27017/cp4', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 });
+
+const cp4Schema = new mongoose.Schema({
+    name: String
+});
+
+const Cp4 = mongoose.model('Cp4', cp4Schema);
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
