90dee6a62bb92a1d9d7d85e81578059391ec6005
Logan Kimball <logankimbs@gmail.com>
2021-04-01 19:23:33 -0600

    Created schema and model for tags

--- back-end/server.js
+++ back-end/server.js
@@ -1,23 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const app = express();
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+// parse application/json
 app.use(bodyParser.json());
 
+// connect to the database
 mongoose.connect('mongodb://localhost:27017/cp4', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-const cp4Schema = new mongoose.Schema({
-    name: String
+// Scheme for tags
+const tagSchema = new mongoose.Schema({
+    item: String,
+    color: String
 });
 
-const Cp4 = mongoose.model('Cp4', cp4Schema);
+// Model for tags
+const Tag = mongoose.model('Tag', tagSchema);
+
+// Create a tag
+app.post('/api/tags', async (req, res) => {
+    const tag = new Tag({
+        item: req.body.item,
+        color: req.body.color
+    });
+    try {
+        await tag.save();
+        res.send(tag);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
