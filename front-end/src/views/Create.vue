0bb1e57a7f1c81cb3855395dccd95b96ab800fc1
Logan Kimball <logankimbs@gmail.com>
2021-04-03 08:59:25 -0600

    Rough build of create page

--- front-end/src/views/Create.vue
+++ front-end/src/views/Create.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="create">
-    <h1>This is the create page</h1>
-  </div>
+    <div class="create">
+        <div class="form">
+            <!-- AUTHOR -->
+            <label for="author">Author:</label> <br>
+            <input> <br><br>
+            
+            <!-- TITLE -->
+            <label for="title">Title:</label> <br>
+            <input> <br><br>
+            
+            <!-- TAG -->
+            <label for="tag">Tag:</label> <br>
+            <input> <br><br>
+            
+            <!-- THUMBNAIL -->
+            <label for="thumbnail">Thumbnail:</label> <br>
+            <input type="file"> <br><br>
+
+            <!-- CONTENT -->
+            <textarea placeholder="Add content..." cols="30" rows="10"></textarea> <br>
+
+            <!-- SUBMIT -->
+            <button>Submit</button>
+        </div>
+    </div>
 </template>
